c691355187e0443ec10bad8603ccd4e2d9a23791
WillzMu <wilfredmulenga@yahoo.com>
2019-08-05 21:14:06 +0200

    fix typo on auth domain

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,9 +4,10 @@ require('firebase/auth')
 require('firebase/database')
 require('firebase/storage')
 
+console.log(process.env.REACT_APP_AUTHDOMAIN)
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
