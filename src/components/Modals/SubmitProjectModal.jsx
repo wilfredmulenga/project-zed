cee3452538f0c28634fe7f39b11762faafab6ecb
WillzMu <wilfredmulenga@yahoo.com>
2019-10-26 22:37:08 +0200

    add form to submit project modal

--- src/components/Modals/SubmitProjectModal.jsx
+++ src/components/Modals/SubmitProjectModal.jsx
@@ -4,8 +4,7 @@ import * as React from 'react'
 import Modal from 'react-modal'
 import { connect } from 'react-redux'
 import ChipInput from 'material-ui-chip-input'
-// import firebase from '../../config/firebase'
-import { toggleSignInModal, logInStateChange, toggleSubmitProjectModal } from '../../actions/actionCreators'
+import { toggleSubmitProjectModal, submitProject } from '../../actions/actionCreators'
 import '../../App.scss'
 
 type Props = {
@@ -42,6 +41,10 @@ class SumbitProjectModal extends React.Component<Props, State> {
     })
   }
 
+  onFormSubmit = (evt) => {
+    evt.preventDefault()
+  }
+
   render () {
     const { isOpen } = this.props
     const { projectOwner, description, link } = this.state
@@ -51,53 +54,74 @@ class SumbitProjectModal extends React.Component<Props, State> {
         style={customStyles}
         contentLabel="submit project modal">
         <div className='submitProjectModal'>
-          <h3>Share your project with the community</h3>
-          <hr/>
-          <div className="inputField">
-            <label>project owner</label>
-            <input
-              placeholder="project owner"
-              type='text'
-              value={projectOwner}
-              onChange={(e) => this.handleInput('projectOwner', e.target.value)}
-              required
-            />
-          </div>
-          <div className="inputField">
-            <label>tools used</label>
-            <ChipInput
-              defaultValue={chipsPlaceholderValues}
-              onChange={(tools) => this.handleToolsChange(tools)}
-            />
-          </div>
-          <div className="inputField">
-            <label>description</label>
-            <textarea
-              placeholder="description"
-              type='text'
-              value={description}
-              onChange={(e) => this.handleInput('description', e.target.value)}
-              required
-            />
-          </div>
-          <div className="inputField">
-            <label>link to projects</label>
-            <input
-              placeholder="link"
-              type='text'
-              value={link}
-              onChange={(e) => this.handleInput('link', e.target.value)}
-              required
-            />
-          </div>
-          <div className='modalCloseButton'>
-            <button
-              onClick={() => this.props.dispatch(toggleSubmitProjectModal())}
-              type="button"
-              className="btn btn-outline-danger">
+          <form onSubmit={this.onFormSubmit}>
+            <h3>Share your project with the community</h3>
+            <hr/>
+            <div className="inputField">
+              <label>project owner</label>
+              <input
+                placeholder="project owner"
+                type='text'
+                value={projectOwner}
+                onChange={(evt) => this.handleInput('projectOwner', evt.target.value)}
+                required
+              />
+            </div>
+            <div className="inputField">
+              <label>tools used</label>
+              <ChipInput
+                defaultValue={chipsPlaceholderValues}
+                onChange={(tools) => this.handleToolsChange(tools)}
+              />
+            </div>
+            <div className="inputField">
+              <label>description</label>
+              <textarea
+                placeholder="description"
+                type='text'
+                value={description}
+                rows={3}
+                onChange={(evt) => this.handleInput('description', evt.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="huey"
+                name="drone"
+                value="huey"
+                checked />
+              <label htmlFor="huey">Huey</label>
+            </div>
+            <div>
+              <input type="radio" id="dewey" name="drone" value="dewey"/>
+              <label htmlFor="dewey">Dewey</label>
+            </div>
+            <div className="inputField">
+              <label>link to projects</label>
+              <input
+                placeholder="link"
+                type='text'
+                value={link}
+                onChange={(evt) => this.handleInput('link', evt.target.value)}
+                required
+              />
+            </div>
+            <div className='modal-button-container'>
+              <button
+                onClick={() => this.props.dispatch(toggleSubmitProjectModal())}
+                type="button"
+                className="btn btn-outline-danger">
             Close
-            </button>
-          </div>
+              </button>
+              <button
+                type="submit"
+                className="btn btn-outline-info">
+            Submit
+              </button>
+            </div>
+          </form>
         </div>
       </Modal>
     )
