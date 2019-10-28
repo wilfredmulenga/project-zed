434716ccd05d56c1e58e3c68580f615923c4d174
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2019-10-27 21:18:52 -0300

    Introduce useState and useEffect hooks into card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,64 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { likeOrDislike, toggleSignInModal } from '../actions/actionCreators'
 
 const whiteBackground = {
   backgroundColor: '#FFF'
-}
+};
 
-class Card extends React.Component {
-  state = {
-    liked: false
-  }
+const Card = ({
+  props: {
+    dispatch,
+    home: {
+      loggedIn,
+    },
+    index,
+    project,
+  },
+}) => {
+  const [liked, setLiked] = useState(false);
 
-  handleCick = (index) => {
-    const { loggedIn } = this.props.home
+  useEffect(() => {
+    dispatch(likeOrDislike(index, liked));
+  });
+
+  const handleClick = () => {
     if (loggedIn) {
-      this.setState({ liked: !this.state.liked },
-        () => this.props.dispatch(likeOrDislike(index, this.state.liked)))
+      setLiked(!liked);
     } else {
-      this.props.dispatch(toggleSignInModal())
+      dispatch(toggleSigninModal());
     }
-  }
+  };
 
-  render () {
-    const { index, project } = this.props
-    const { liked } = this.state
-    return (
-      <div key={index} className="row justify-content-center">
-        <div className="col-md-8 col-sm-8 mb-4">
-          <div>
-            <div className="card-body row" style={whiteBackground}>
-              <div className="col-5">
-                <p style={{ display: 'inline', marginBottom: 20 }} className="card-title">{project.githubUsername}</p>
-                <p>
-              Tools:
-                  {' '}
-                  {project.tools.map((element, i) => (<span key={i} className="card-subtitle mr-1">{element}</span>))}
-                </p>
-                <p>{project.type}</p>
-                <div className='like-container'>
-                  <div className="like" onClick={() => this.handleCick(index)}><p>{liked ? 'unlike' : 'like' }</p></div>
-                  <div className='like-count'>{project.likes}</div>
-                </div>
+  return (
+    <div key={index} className="row justify-content-center">
+      <div className="col-md-8 col-sm-8 mb-4">
+        <div>
+          <div className="card-body row" style={whiteBackground}>
+            <div className="col-5">
+              <p style={{ display: 'inline', marginBottom: 20 }} className="card-title">{project.githubUsername}</p>
+              <p>
+            Tools:
+                {' '}
+                {project.tools.map((element, i) => (<span key={i} className="card-subtitle mr-1">{element}</span>))}
+              </p>
+              <p>{project.type}</p>
+              <div className='like-container'>
+                <div className="like" onClick={() => this.handleCick(index)}><p>{liked ? 'unlike' : 'like' }</p></div>
+                <div className='like-count'>{project.likes}</div>
               </div>
-              <div className="col-7">
-                <p className='remove-overflow-text' style={{ display: 'inline', marginBottom: 20 }}>{project.description}</p>
-                <p>
-                  {' '}
-              Link:
-                  {' '}
-                  <a className='remove-overflow-text' rel="noopener noreferrer" target="_blank" href={project.link}>{project.link}</a>
-                  {' '}
-                </p>
-              </div>
+            </div>
+            <div className="col-7">
+              <p className='remove-overflow-text' style={{ display: 'inline', marginBottom: 20 }}>{project.description}</p>
+              <p>
+                {' '}
+            Link:
+                {' '}
+                <a className='remove-overflow-text' rel="noopener noreferrer" target="_blank" href={project.link}>{project.link}</a>
+                {' '}
+              </p>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 function mapStateToProps ({ homeReducer }) {
   return {
