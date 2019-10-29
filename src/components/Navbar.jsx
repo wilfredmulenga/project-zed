c7422991bd0379a16a265bbe1582d5b64970f303
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2019-10-28 21:29:20 -0300

    Make Navbar a functional component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,34 +2,40 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { toggleSignInModal, toggleSignOutModal } from '../actions/actionCreators'
 
-class Navbar extends React.Component {
-  render () {
-    const { loggedIn } = this.props.home
-    return (
-      <div>
-        <nav className="navbar navbar-dark bg-dark">
-          <a className="navbar-brand" style={{ color: '#FFF' }} href='/'>Project Zed</a>
-          <form className="form-inline">
-            <button
-              onClick={(e) => {
-                e.preventDefault()
-                loggedIn ? this.props.dispatch(toggleSignOutModal()) : this.props.dispatch(toggleSignInModal())
-              }}
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit">
-              { loggedIn ? `Sign Out` : `Sign In` }
-            </button>
-          </form>
-        </nav>
-      </div>
-    )
-  }
-}
+const Navbar = ({
+  home: {
+    loggedIn,
+  },
+  toggleSignInModal,
+  toggleSignOutModal,
+}) => {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" style={{ color: '#FFF' }} href='/'>Project Zed</a>
+        <form className="form-inline">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              loggedIn ? toggleSignOutModal() : toggleSignInModal();
+            }}
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit">
+            { loggedIn ? 'Sign Out' : 'Sign In' }
+          </button>
+        </form>
+      </nav>
+    </div>
+  );
+};
 
-function mapStateToProps ({ homeReducer }) {
+const mapStateToProps = ({ homeReducer }) => {
   return {
-    home: homeReducer
-  }
-}
+    home: homeReducer,
+  };
+};
 
-export default connect(mapStateToProps, null)(Navbar)
+export default connect(
+  mapStateToProps,
+  { toggleSignInModal, toggleSignOutModal },
+)(Navbar);
