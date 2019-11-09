e93950a578b3e0932a103b56b25e26423d1a0f46
WillzMu <wilfredmulenga@yahoo.com>
2019-11-10 00:44:15 +0200

    add types

--- src/types/types.js
+++ src/types/types.js
@@ -5,3 +5,17 @@ export type Home = {
   loggedIn: boolean,
   submitProjectModalOpen: boolean
 }
+
+export type Dispatch = (any) => void
+
+export type Project = {
+  projectOwner: string,
+  tools: Array<string>,
+  description: string,
+  typeOfProject: string,
+  link: string,
+  loading: boolean,
+  responseMessage: string,
+  submitted: boolean,
+  projectId: ''
+}
